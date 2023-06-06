46e1d1ab1a9047b5bbec6cdf37e5fd59c9c15565
Carlos Veny <carlosveny@yahoo.es>
2023-06-06 22:13:05 +0200

    test: deploy endpoint

--- src/app/index.js
+++ src/app/index.js
@@ -6,24 +6,7 @@ const FUEL_STATIONS_URL = 'https://sedeaplicaciones.minetur.gob.es/ServiciosREST
 const EESS_IDS = ['2651']
 
 APP.get('/', (req, res) => {
-  https.get(FUEL_STATIONS_URL, (resp) => {
-    let data = '';
-
-    // Un fragmento de datos ha sido recibido.
-    resp.on('data', (chunk) => {
-      data += chunk;
-    });
-
-    resp.on('end', () => {
-      const fuelData = JSON.parse(data).ListaEESSPrecio
-      // const filtered = fuelData.filter(fuelStation => fuelStation.Provincia === 'BALEARS (ILLES)'
-      //   && fuelStation['Rótulo'] === 'REPSOL')
-      const filtered = fuelData.filter(fuelStation => EESS_IDS.includes(fuelStation.IDEESS))
-      console.log(filtered);
-      res.send(filtered)
-    });
-
-  })
+  res.send('hola funciona')
 })
 
 
