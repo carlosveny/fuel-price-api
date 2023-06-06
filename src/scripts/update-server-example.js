1c01c55f4f7a44899b6af40379b49f2635eacb43
Carlos Veny <carlosveny@yahoo.es>
2023-06-06 21:39:20 +0200

    feat: added start script

--- src/scripts/update-server-example.js
+++ src/scripts/update-server-example.js
@@ -1,9 +1,9 @@
 const axios = require('axios')
 
-const args = process.argv.slice(2)
-const url = `URL_TO_YOUR_DEPLOY_SERVER?repo=${args[0]}`
+const ARGS = process.argv.slice(2)
+const URL = `URL_TO_YOUR_DEPLOY_SERVER?repo=${ARGS[0]}`
 
-console.log(`Updating repo "${args[0]}"`)
-axios.get(url)
+console.log(`Updating repo "${ARGS[0]}"`)
+axios.get(URL)
   .then((response) => console.log('\x1b[32m%s\x1b[0m', response.data))
   .catch((error) => console.error('\x1b[31m%s\x1b[0m', `${error.response.data}\n`))
