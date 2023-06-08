0a4cc80201de122f8408b5face5e1558c98a4a00
Carlos Veny <carlosveny@yahoo.es>
2023-06-08 20:22:36 +0200

    test 10

--- src/app/index.js
+++ src/app/index.js
@@ -6,7 +6,7 @@ const FUEL_STATIONS_URL = 'https://sedeaplicaciones.minetur.gob.es/ServiciosREST
 const EESS_IDS = ['2651']
 
 APP.get('/', (req, res) => {
-  res.send('hola funciona9')
+  res.send('hola funciona10')
   return
 })
 
