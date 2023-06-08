b747cd7e9d53a48fa72526ca68bbbb18b37f658d
Carlos Veny <carlosveny@yahoo.es>
2023-06-08 23:21:30 +0200

    feat: first request

--- src/app/index.js
+++ src/app/index.js
@@ -1,13 +1,18 @@
 const express = require('express')
-const https = require('https');
+const axios = require('axios')
+
 const APP = express()
 const PORT = 3000
 const FUEL_STATIONS_URL = 'https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/'
 const EESS_IDS = ['2651']
 
 APP.get('/', (req, res) => {
-  res.send('hola funciona10')
-  return
+  axios.get(FUEL_STATIONS_URL).then(response => {
+    const fuelStationsList = response.data.ListaEESSPrecio
+    console.log(fuelStationsList[0])
+    res.send(fuelStationsList[0])
+    return
+  })
 })
 
 
